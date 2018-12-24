183a248452226aa7c51aae56be1c6944fa226086
Don Chatelain <djchatelain@gmail.com>
2018-12-23 20:31:44 -0800

    fixes something

--- scripts/photon.js
+++ scripts/photon.js
@@ -2,15 +2,6 @@
 
 const component = document.getElementById('soil-read-container');
 
-const particle = new Particle();
-const username = PARTICLE_USER;
-const password = PARTICLE_PASS;
-let token;
-
-particle
-  .login({ username, password })
-  .then(data => token = data.body.access_token), err => console.error('login', err);
-
 const getSoilReading = () => {
   fetch("https://api.particle.io/v1/devices/3f0023000547353138383138/getMoisture?access_token=f50e6bd841e35fd9b8366ce7f823561d92ef32f1 -d arg=''")
     .then(res => {
